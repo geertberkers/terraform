3b387fa2e8b9e85069d1632bcdb8511fb58b7b38
Geert <geertberkers@hotmail.com>
2026-04-03 00:35:25 +0200

    Added output

--- modules/databases/outputs.tf
+++ modules/databases/outputs.tf
@@ -1,15 +1,20 @@
-output "postgres_fqdn" {
-  value = azurerm_postgresql_flexible_server.postgres.fqdn
-}
-
 output "mysql_fqdn" {
   value = azurerm_mysql_flexible_server.mysql.fqdn
 }
 
-output "sql_server_name" {
-  value = azurerm_mssql_server.sql.name
+output "postgres_fqdn" {
+  value = azurerm_postgresql_flexible_server.postgres.fqdn
+}
+
+output "sql_server_fqdn" {
+  value = azurerm_mssql_server.sql.fully_qualified_domain_name
 }
 
 output "cosmos_endpoint" {
   value = azurerm_cosmosdb_account.cosmos.endpoint
+}
+
+output "cosmos_primary_key" {
+  value     = azurerm_cosmosdb_account.cosmos.primary_key
+  sensitive = true
 }
