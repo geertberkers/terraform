455efbdd6a0bd6513e1ae63b322e2e37ce228f91
Geert <geertberkers@hotmail.com>
2026-04-02 23:19:52 +0200

    Update cosmos.tf

--- modules/databases/cosmos.tf
+++ modules/databases/cosmos.tf
@@ -32,5 +32,5 @@ resource "azurerm_cosmosdb_sql_container" "container" {
   account_name        = azurerm_cosmosdb_account.cosmos.name
   database_name       = azurerm_cosmosdb_sql_database.db.name
 
-  partition_key_path = "/id"
+  partition_key_paths = ["/id"]
 }
