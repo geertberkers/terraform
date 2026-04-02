ba0b7576d57380c9adef5837cecdf4376f4af1b5
Geert <geertberkers@hotmail.com>
2026-04-02 23:07:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,6 +80,9 @@ module "databases" {
   mysql_admin_user     = var.mysql_admin_user
   mysql_admin_password = var.mysql_admin_password
 
-  sql_admin_user     = var.sql_admin_user
-  sql_admin_password = var.sql_admin_password
+  sql_admin_user       = var.sql_admin_user
+  sql_admin_password   = var.sql_admin_password
+
+  pg_admin_user       = var.pg_admin_user
+  pg_admin_password   = var.pg_admin_password
 }
