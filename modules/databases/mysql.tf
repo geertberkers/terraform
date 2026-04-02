f2cfaf41276b780755aeb37b80d65310973df57a
Geert <geertberkers@hotmail.com>
2026-04-03 00:09:40 +0200

    Update mysql.tf

--- modules/databases/mysql.tf
+++ modules/databases/mysql.tf
@@ -17,23 +17,5 @@ resource "azurerm_mysql_flexible_server" "mysql" {
 
   # required in newer provider versions
   # Optional but recommended: zone can cause issues in some regions
-  zone = "1"
-}
-
-# NOTE:
-# azurerm_mysql_flexible_database DOES NOT EXIST in azurerm provider.
-# Databases must be created manually or via SQL execution.
-
-# Optional: create database via init script (only works if mysql client available)
-resource "null_resource" "mysql_init_db" {
-  depends_on = [azurerm_mysql_flexible_server.mysql]
-
-  provisioner "local-exec" {
-    command = <<EOT
-mysql -h ${azurerm_mysql_flexible_server.mysql.fqdn} \
-      -u ${var.mysql_admin_user} \
-      -p${var.mysql_admin_password} \
-      -e "CREATE DATABASE IF NOT EXISTS appdb;"
-EOT
-  }
+  # zone = "1"
 }
