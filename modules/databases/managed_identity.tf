9bc176ccf58bd3f4d24ff18ce89d3163585e9964
Geert <geertberkers@hotmail.com>
2026-04-07 15:34:15 +0200

    Update managed_identity.tf
    Chagne object id

--- modules/databases/managed_identity.tf
+++ modules/databases/managed_identity.tf
@@ -5,7 +5,7 @@ resource "azurerm_postgresql_flexible_server_active_directory_administrator" "po
   server_name         = azurerm_postgresql_flexible_server.postgres.name
   resource_group_name = var.resource_group_name
   tenant_id           = data.azurerm_client_config.current.tenant_id
-  object_id           = var.app_identity_principal_id
+  object_id           = data.azurerm_linux_web_app.app.identity[0].principal_id
   principal_type      = "ServicePrincipal"
   principal_name      = "app-identity"
 }
@@ -50,8 +50,7 @@ resource "null_resource" "sql_contained_user" {
 resource "azurerm_key_vault_access_policy" "app_identity_kv" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = var.app_identity_principal_id
-
+  object_id    = data.azurerm_linux_web_app.app.identity[0].principal_id
   secret_permissions = [
     "Get", "List"
   ]
