f32ab703953d7cd81910c05c55b2d0804a7e523e
Geert <geertberkers@hotmail.com>
2026-04-07 10:02:13 +0200

    Update postgres.tf
    Enabled active_directory_auth_enabled

--- modules/databases/postgres.tf
+++ modules/databases/postgres.tf
@@ -6,6 +6,7 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
   administrator_login    = var.pg_admin_user
   administrator_password = random_password.pg_admin.result
 
+  active_directory_auth_enabled = true
   sku_name = "B_Standard_B1ms"
 
   storage_mb = 32768
@@ -16,7 +17,10 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
   public_network_access_enabled = true
 
   lifecycle {
-    ignore_changes = [zone]
+    ignore_changes = [
+      zone,
+      high_availability
+    ]
   }
 }
 
