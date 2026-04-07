1039e3ecaa2b555f49b20bba4bb13252a1755eb1
Geert <geertberkers@hotmail.com>
2026-04-07 15:53:58 +0200

    Update managed_identity.tf

--- modules/databases/managed_identity.tf
+++ modules/databases/managed_identity.tf
@@ -55,7 +55,9 @@ resource "null_resource" "sql_contained_user" {
 resource "azurerm_key_vault_access_policy" "app_identity_kv" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_linux_web_app.app.identity[0].principal_id
+
+  object_id = azurerm_user_assigned_identity.app_identity.principal_id
+
   secret_permissions = [
     "Get", "List"
   ]
