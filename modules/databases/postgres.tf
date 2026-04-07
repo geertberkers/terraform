e7e9ebb1fdfb11252c142ec689441ce5382ba583
Geert Berkers <geertberkers@hotmail.com>
2026-04-07 08:14:09 +0000

    terraform fmt

--- modules/databases/postgres.tf
+++ modules/databases/postgres.tf
@@ -6,7 +6,7 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
   administrator_login    = var.pg_admin_user
   administrator_password = random_password.pg_admin.result
 
-  sku_name                      = "B_Standard_B1ms"
+  sku_name = "B_Standard_B1ms"
 
   storage_mb = 32768
   version    = "14"
