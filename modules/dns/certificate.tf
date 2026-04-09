c271156e31e55cb05769795d865ad8e1c1f743c6
Geert <geertberkers@hotmail.com>
2026-04-09 14:23:10 +0200

    Update certificate.tf

--- modules/dns/certificate.tf
+++ modules/dns/certificate.tf
@@ -3,7 +3,7 @@ resource "azurerm_app_service_managed_certificate" "managed_cert" {
 }
 
 resource "azurerm_app_service_certificate_binding" "cert_binding" {
-  hostname_binding_id      = azurerm_app_service_custom_hostname_binding.custom_domain.id
-  certificate_binding_type = "SNI SSL"
-  ssl_certificate_id       = azurerm_app_service_managed_certificate.managed_cert.id
+  hostname_binding_id = azurerm_app_service_custom_hostname_binding.custom_domain.id
+  certificate_id      = azurerm_app_service_managed_certificate.managed_cert.id
+  ssl_state           = "SniEnabled"
 }
