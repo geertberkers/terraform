c78c333136bc62a0a0bcb0b60f49eeabdefde4ff
Geert <geertberkers@hotmail.com>
2026-04-09 10:33:52 +0200

    Update backend.tf
    Undo update rg name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-tfstate"
+    resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstate12792"
     container_name       = "tfstate"
     key                  = "multi-cloud.tfstate"
