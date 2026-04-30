86873d80d04046c95d2489b0404d103e7d79b4ba
Geert <geertberkers@hotmail.com>
2026-04-30 08:42:59 +0200

    Update variables.tf
    Removed duplicated variables

--- modules/databases/variables.tf
+++ modules/databases/variables.tf
@@ -26,33 +26,6 @@ variable "sql_database_name" {
   type = string
 }
 
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "env" {
-  type = string
-}
-
-variable "mysql_admin_user" {
-  type = string
-}
-
-variable "sql_admin_user" {
-  type = string
-}
-
-variable "pg_admin_user" {
-  type = string
-}
-
-variable "sql_database_name" {
-  type = string
-}
 
 variable "app_identity_principal_id" {
   type        = string
